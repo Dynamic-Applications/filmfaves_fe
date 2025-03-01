406f3cf0a28942ecfd7665b66ffcc6f62b9a7ebd
Mirage Kamran <mirage.kamran.pkt@gmail.com>
2025-03-01 15:36:14 -0800

    reset token should work in ui

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -34,8 +34,7 @@ export default function ResetPassword() {
 
         try {
             const response = await fetch(
-                `${API_URL}/passresets/reset-password` ||
-                    "http://localhost:4000/passresets/reset-password",
+                `${API_URL}/reset-password`,
                 {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
